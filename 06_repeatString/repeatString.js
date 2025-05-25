b5c9b3d0cb1bab80569be5071ea29c9b168422d6
avik-ch <181900631+avik-ch@users.noreply.github.com>
2025-05-25 17:56:37 -0500

    Complete exercise 6

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, times) {
+    if (times < 0) {
+        return 'ERROR';
+    }
+    let ans = '';
+    for (let i = 0; i < times; i++) {
+        ans += str;
+    }
+    return ans;
 };
 
 // Do not edit below this line
