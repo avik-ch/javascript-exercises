a4fcbb796402d58392f408ad42694be85a736863
avik-ch <181900631+avik-ch@users.noreply.github.com>
2025-05-25 18:44:56 -0500

    Complete exercise 07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    // return str.split('').reverse().join('');
+    let ans = '';
+    for (let i = 0; i < str.length; i++) {
+        ans = str.charAt(i) + ans;
+    }
+    return ans;
 };
 
 // Do not edit below this line
