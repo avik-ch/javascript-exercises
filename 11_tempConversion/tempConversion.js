d7dbda8d57e298e32da2db48f28806211af1a8ae
avik-ch <181900631+avik-ch@users.noreply.github.com>
2025-05-25 19:38:38 -0500

    Complete exercise 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,8 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInFahrenheit) {
+  return roundToDecimal((tempInFahrenheit - 32) * (5 / 9), 1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInCelsius) {
+  return roundToDecimal(tempInCelsius * (9 / 5) + 32, 1);
 };
+
+function roundToDecimal(val, dp) {
+  const factor = 10 ** dp;
+  return Math.round((val * factor)) / factor;
+}
 
 // Do not edit below this line
 module.exports = {
