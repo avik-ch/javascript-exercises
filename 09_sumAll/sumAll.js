d54858966e173bc24ed01d104723bdd8ac21f0e1
avik-ch <181900631+avik-ch@users.noreply.github.com>
2025-05-25 19:24:01 -0500

    Complete exercise 9

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (Math.min(a, b) < 0 || !Number.isInteger(a) || !Number.isInteger(b)) return 'ERROR';
 
+    if (a > b) [a, b] = [b, a];
+
+    let total = 0;
+    for (let i = a; i <= b; i++) {
+        total += i;
+    }
+    return total;
 };
 
 // Do not edit below this line
