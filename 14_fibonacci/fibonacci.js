4cfa59def8669ce16fb9fed6bcf8d1fe1dbde6c8
avik-ch <181900631+avik-ch@users.noreply.github.com>
2025-05-31 19:46:34 -0500

    Finish exercise 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) return "OOPS";
+    if (num == 0) return 0;
+    if (num < 3) return 1;
+    let val1 = val2 = 1;
+    for (let i = 2; i < num; i++) {
+        [val2, val1] = [val1 + val2, val2];
+    }
+    return val2;
 };
 
 // Do not edit below this line
