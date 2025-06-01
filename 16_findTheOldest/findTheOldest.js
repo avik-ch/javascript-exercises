ac1c8d5c447b04363958addc4306169d58640884
avik-ch <181900631+avik-ch@users.noreply.github.com>
2025-05-31 20:03:46 -0500

    Finish exercise 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+function getAge(birth, death) {
+    if (death === undefined) {
+        death = 2025;
+    }
+    return death - birth;
+}
 
+const findTheOldest = function(arr) {
+    return arr.reduce((acc, curr) => {
+        return getAge(curr.yearOfBirth, curr.yearOfDeath) > getAge(acc.yearOfBirth, acc.yearOfDeath) ? curr : acc;
+    });
 };
 
 // Do not edit below this line
